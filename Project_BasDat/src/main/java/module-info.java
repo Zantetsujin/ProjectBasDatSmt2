5806ca83cf6a4dfed4cfa265a8c2a3bf34d3a5b7
Cadip <131934036+Cadip@users.noreply.github.com>
2023-06-12 18:54:25 +0700

    Delete Project_BasDat directory

--- Project_BasDat/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.project_basdat {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.project_basdat to javafx.fxml;
-    exports com.example.project_basdat;
-}
